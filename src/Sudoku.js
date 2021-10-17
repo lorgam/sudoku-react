46ce057c9f41da3226853facb30c94c07d66dd18
Luis A. Martín <lorgam@gmail.com>
2021-10-17 13:07:10 +0100

    Sudoku component

---
+++ src/Sudoku.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sudoku.css';
+
+class Sudoku extends React.Component {
+  render(){
+    return (
+      <h1>Sudoku</h1>
+    );
+  }
+}
+
+export default Sudoku;
